ef68382ca8e7e37fb042afe214dda91f933af8ca
Habidhossen <habidhossen2@gmail.com>
2023-09-13 00:07:34 +0600

    code up[dated

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,5 +1,5 @@
 const BookCard = ({ book }) => {
-  const { Title, Author, Genre, PublicationDate, CoverImage } = book;
+  const { title, author, genre, publicationDate, coverImage } = book;
 
   return (
     <div className="p-4 lg:w-1/5 md:w-1/2">
@@ -7,15 +7,15 @@ const BookCard = ({ book }) => {
         <img
           alt="team"
           className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-          src={CoverImage}
+          src={coverImage}
         />
         <div className="w-full">
           <h2 className="title-font font-medium text-lg text-gray-900">
-            {Title}
+            {title}
           </h2>
-          <h3 className="text-gray-500 mb-3">{Author}</h3>
-          <p className="">{Genre}</p>
-          <p className="mb-4">{PublicationDate}</p>
+          <h3 className="text-gray-500 mb-3">{author}</h3>
+          <p className="">{genre}</p>
+          <p className="mb-4">{publicationDate}</p>
         </div>
       </div>
     </div>
