103b58f0881f9c3a10398bfd5f65d66a848072c1
Habidhossen <habidhossen2@gmail.com>
2023-09-13 10:18:58 +0600

    review star func added

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -166,9 +166,28 @@ const BookDetails = () => {
                     <span className="block text-gray-700 font-medium">
                       {review?.username}
                     </span>
-                    <span className="block text-gray-700 text-sm">
-                      Rating: {review?.rating} star
-                    </span>
+                    <div className="flex items-center gap-3">
+                      {/* render star based on ratings */}
+                      <span className="flex items-center">
+                        {Array.from({ length: 5 }, (_, index) => (
+                          <svg
+                            key={index}
+                            fill={
+                              index < review?.rating ? "currentColor" : "none"
+                            }
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-4 h-4 text-purple-500"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                          </svg>
+                        ))}
+                      </span>
+                      {review?.rating} out of 5
+                    </div>
                   </div>
                 </div>
                 <div>
