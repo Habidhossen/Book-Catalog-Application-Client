7fff466cd2bb9891ce3743267699860280a008c6
Habidhossen <habidhossen2@gmail.com>
2023-09-14 10:44:34 +0600

    remove from wishlist feature added

--- src/pages/WishList.tsx
+++ src/pages/WishList.tsx
@@ -1,5 +1,34 @@
+import { removeFromWishlist } from "../redux/features/books/wishlistSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hook";
+import { Book } from "../types/globalTypes";
+
 const WishList = () => {
-  return <div>Empty Wishlist</div>;
+  const dispatch = useAppDispatch();
+  const wishlist = useAppSelector((state) => state.wishlist);
+
+  // handle remove from wishlist button
+  const handleRemoveFromWishlist = (book: Book) => {
+    dispatch(removeFromWishlist(book));
+  };
+
+  return (
+    <div>
+      {wishlist?.books?.length > 0 ? (
+        <div className="container container-lg mx-auto">
+          {wishlist?.books?.map((book) => (
+            <div key={book._id}>
+              <h1>{book.title}</h1>
+              <button onClick={() => handleRemoveFromWishlist(book)}>
+                Delete from Wishlist
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>Your Wishlist is empty!</div>
+      )}
+    </div>
+  );
 };
 
 export default WishList;
