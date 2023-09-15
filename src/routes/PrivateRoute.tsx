da5aa39699652e382e329ef97f1e869c63f78f2a
Habidhossen <habidhossen2@gmail.com>
2023-09-16 03:32:47 +0600

    Private Route added

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAppSelector } from "../redux/hook";
+
+interface IProps {
+  children: ReactNode;
+}
+
+const PrivateRoute = ({ children }: IProps) => {
+  // get user from Redux State
+  const { user, isLoading } = useAppSelector((state) => state.user);
+
+  // get current location by React useLocation Hook
+  const location = useLocation();
+
+  // is loading...
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  // checking...
+  if (!user.email) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
